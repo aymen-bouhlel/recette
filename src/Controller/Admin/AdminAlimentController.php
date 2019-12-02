ca872770c44332a8c93518227ffc7ec6ecfdbd76
Bouhlel El Bechri <bouhlel_elbechri@web.de>
2019-12-02 13:29:09 +0100

    Modification d'aliment: creation de formulaire de modification + configuration de fichier twig.yaml

--- src/Controller/Admin/AdminAlimentController.php
+++ src/Controller/Admin/AdminAlimentController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Aliment;
+use App\Form\AlimentType;
 use App\Repository\AlimentRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminAlimentController extends AbstractController
 {
@@ -17,6 +19,19 @@ class AdminAlimentController extends AbstractController
 
         return $this->render('admin/admin_aliment/adminAliment.html.twig', [
             'aliments' => $aliments,
+        ]);
+    }
+
+    /**
+     * @Route("/admin/aliment/{id}", name="admin_aliment_modification")
+     */
+    public function modification(Aliment $aliment)
+    {
+        $form = $this->createForm(AlimentType::class, $aliment);
+
+        return $this->render('admin/admin_aliment/modifiacationAliment.html.twig', [
+            'aliment' => $aliment,
+            'formAliment' => $form->createView(),
         ]);
     }
 }
