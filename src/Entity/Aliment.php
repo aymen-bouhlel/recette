d6bc9c736d16f0ef2b0f4b25b0f219150667b0b4
Bouhlel El Bechri <bouhlel_elbechri@web.de>
2019-12-03 11:47:15 +0100

    Activer le VichBundle dans l'entiy Eliment + misà jour de formulaire d'ajout et modification d'un aliment

--- src/Entity/Aliment.php
+++ src/Entity/Aliment.php
@@ -4,9 +4,11 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\AlimentRepository")
+ * @vich\Uploadable
  */
 class Aliment
 {
@@ -43,6 +45,15 @@ class Aliment
      * @ORM\Column(type="string", length=255)
      */
     private $image;
+
+    /**
+     * NOTE: This is not a mapped field of entity metadata, just a simple property.
+     * 
+     * @Vich\UploadableField(mapping="aliment_image", fileNameProperty="image")
+     * 
+     * @var File
+     */
+    private $imageFile;
 
     /**
      * @ORM\Column(type="integer")
@@ -103,6 +114,32 @@ class Aliment
         $this->image = $image;
 
         return $this;
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile $imageFile
+     */
+    public function setImageFile(?File $imageFile = null): self
+    {
+        $this->imageFile = $imageFile;
+        return $this;
+
+        // if (null !== $imageFile) {
+        //     // It is required that at least one field changes if you are using doctrine
+        //     // otherwise the event listeners won't be called and the file is lost
+        //     $this->updatedAt = new \DateTimeImmutable();
+        // }
     }
 
     public function getCalorie(): ?int
